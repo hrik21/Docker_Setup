431ad1af635e5427b98f62697ef97f77afa7c5fc
HRITHIK PURI <89735378+hrik21@users.noreply.github.com>
2025-06-24 00:44:38 -0400

    Add CI/CD pipeline

---
+++ test/test.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+# Add the parent directory to sys.path so app.py can be imported
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from app import app  # now this will work
+
+def test_home():
+    with app.test_client() as client:
+        response = client.get('/')
+        assert response.status_code == 200
+        assert b"Hello Welcome" in response.data
